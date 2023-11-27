594897f62e1a487b1b6092c6468e244bc0155e46
GeeseAndQuack <jack.comber@gmail.com>
2023-11-27 23:30:14 +0000

    Let's geddit

--- This_wan.py
+++ This_wan.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import model_selection
 from sklearn import linear_model
+from sklearn import preprocessing
 
 event_data = pd.read_csv("C:/Users/Jack/Documents/Coding Projects/UFC/Data/ufc_event_data.csv")
 fight_data = pd.read_csv("C:/Users/Jack/Documents/Coding Projects/UFC/Data/ufc_fight_data.csv")
@@ -24,27 +25,49 @@ def row_sum_ignore_na(row: pd.Series, cols: list) -> float:
             res += row[i]
     return res
 
+def min_sec_to_sec(row: pd.Series, time: str) -> float:
+    try:
+        mins_secs = row[time].split(":")
+    except:
+        return np.nan
+    else:
+        if len(mins_secs) == 2:
+            mins = int(mins_secs[0]) * 60
+            secs = int(mins_secs[1])
+            res = mins + secs
+            return res
+        else:
+            return np.nan
+
 df = fight_stat_data.merge(fighter_data, 
     how = "left", 
     left_on = "fighter_id", 
     right_on = "fighter_id")
 
-
 df["strike_succ_prcnt"] = stat_prcnt_calc(df,"total_strikes_att", "total_strikes_succ")
 df["sig_strike_succ_prcnt"] = stat_prcnt_calc(df,"sig_strikes_att", "sig_strikes_succ")
 df["takedown_succ_prcnt"] = stat_prcnt_calc(df,"takedown_att", "takedown_succ")
 
+# Calculating total number of fights - using row_sum_ignore_na
 df["tot_fights"] = df.apply(
     lambda row: row_sum_ignore_na(
         row, ["fighter_w", "fighter_l", "fighter_d", "fighter_nc_dq"]),
          axis = 1)
 
+# Calculating total ufc fights by counting rows when grouping by fighter_id
 tot_ufc_fights = df.groupby("fighter_id").size().reset_index(name="Count")
-
+# Joining with fighter_data to get name for fighter
 tot_ufc_fights = tot_ufc_fights.merge(fighter_data[["fighter_id","fighter_f_name", "fighter_l_name"]], how = "left", left_on = "fighter_id", right_on = "fighter_id")
 
-df.dropna(subset = ["fighter_f_name", "fighter_l_name"], inplace = True)
+# Converting ctrl_time and finish_time into a integer representation as seconds
+df["ctrl_time_sec"] = df.apply(
+    lambda row: min_sec_to_sec(row, "ctrl_time"), axis = 1
+)
+df["finish_time_sec"] = df.apply(
+    lambda row: min_sec_to_sec(row, "finish_time"), axis = 1
+)
 
+# Raw winner variable is as the fighter_id, want to conver this to a binary when joint on fighter_data
 wins = fight_data[["fight_id", 
                   "event_id", 
                   "winner", 
